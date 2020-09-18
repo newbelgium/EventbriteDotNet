91e0e4578f257cc1c124a6d57188ca1b91016963
khnbb <kherrmann@newbelgium.com>
2020-09-18 16:45:55 -0600

    adds currents fields to image object

--- EventbriteNET/Image.cs
+++ EventbriteNET/Image.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace EventbriteNET
 {
     /// <summary>
-    /// Represents an Eventbrite imgage <see cref="https://www.eventbrite.com/developer/v3/response_formats/image/#ebapi-std:format-image"/>
+    /// Represents an Eventbrite image <see cref="https://www.eventbrite.com/developer/v3/response_formats/image/#ebapi-std:format-image"/>
     /// </summary>
     public class Image : EventbriteObject
     {
@@ -19,5 +19,56 @@ namespace EventbriteNET
 
         [JsonProperty("url")]
         public string Url { get; set; }
+
+        [JsonProperty("crop_mask")]
+        public CropMask CropMask { get; set; }
+
+        [JsonProperty("original")]
+        public Original Original { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("aspect_ratio")]
+        public long AspectRatio { get; set; }
+
+        [JsonProperty("edge_color")]
+        public string EdgeColor { get; set; }
+
+        [JsonProperty("edge_color_set")]
+        public bool EdgeColorSet { get; set; }
+    }
+
+    public partial class CropMask
+    {
+        [JsonProperty("top_left")]
+        public TopLeft TopLeft { get; set; }
+
+        [JsonProperty("width")]
+        public long Width { get; set; }
+
+        [JsonProperty("height")]
+        public long Height { get; set; }
+    }
+
+    public partial class TopLeft
+    {
+        [JsonProperty("x")]
+        public long X { get; set; }
+
+        [JsonProperty("y")]
+        public long Y { get; set; }
+    }
+
+    public partial class Original
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("width")]
+        public long Width { get; set; }
+
+        [JsonProperty("height")]
+        public long Height { get; set; }
     }
 }
